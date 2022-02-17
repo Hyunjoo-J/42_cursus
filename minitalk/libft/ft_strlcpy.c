04ef0b7033b942113e42dc57e572be8f67b928c1
Hyunjoo <jhj2844@naver.com>
2022-02-17 23:11:59 +0900

    1st commit

---
+++ minitalk/libft/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyjeong <hyjeong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 13:57:33 by hyjeong           #+#    #+#             */
+/*   Updated: 2021/07/07 16:37:34 by hyjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t len)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (!dst || !src)
+		return (0);
+	if (len > 0)
+	{
+		while (--len && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
---
+++ minitalk/server.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 21:30:53 by hyunjoo           #+#    #+#             */
+/*   Updated: 2022/02/17 23:10:30 by hyunjoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	pid_t	server_pid;
+	struct sigaction	act;
+
+	server_pid = getpid();
+	ft_putstr_fd("Server pid : ", 1);
+	ft_putnbr_fd(server_pid, 1);
+	ft_putstr_fd("\n", 1);
+	act.sa_flags = ;
+	act.sa_
+	return (0);
+}
