c418b46da3a0a3feed20f08da99e1ab8c3c4df6c
Hyunjoo <jhj2844@naver.com>
2022-02-20 23:39:27 +0900

    2nd

---
+++ minitalk/client.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 21:49:05 by hyunjoo           #+#    #+#             */
+/*   Updated: 2022/02/20 22:38:00 by hyunjoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+#endif
---
+++ minitalk/server.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 21:51:29 by hyunjoo           #+#    #+#             */
+/*   Updated: 2022/02/20 22:38:01 by hyunjoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+#endif
