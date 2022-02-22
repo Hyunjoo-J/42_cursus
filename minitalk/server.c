f969ecda829942a6e1a09338771af59fea2e65e8
Hyunjoo <jhj2844@naver.com>
2022-02-22 21:48:18 +0900

    2nd

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 21:30:53 by hyunjoo           #+#    #+#             */
-/*   Updated: 2022/02/20 22:37:37 by hyunjoo          ###   ########.fr       */
+/*   Updated: 2022/02/22 21:48:10 by hyunjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int main()
 	server_pid = getpid();
 	ft_putstr_fd("Server pid : ", 1);
 	ft_putnbr_fd(server_pid, 1);
-	ft_putstr_fd("\n", 1);
+	write(1, "\n", 1);
 	act.sa_flags = ;
-	act.sa_
+	act.sa_mask
 	return (0);
 }
