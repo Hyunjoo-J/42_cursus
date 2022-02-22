d2708172511c3273e5e2dab0032399576edcc7ff
Hyunjoo <jhj2844@naver.com>
2022-02-23 05:39:31 +0900

    Mandatory

--- minitalk/client.c
+++ minitalk/client.c
@@ -5,13 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/17 21:30:46 by hyunjoo           #+#    #+#             */
-/*   Updated: 2022/02/20 23:39:14 by hyunjoo          ###   ########.fr       */
+/*   Created: 2022/02/23 04:02:03 by hyunjoo           #+#    #+#             */
+/*   Updated: 2022/02/23 05:06:19 by hyunjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "client.h"
+
+void	send(pid_t pid, int num)
+{
+	if (num == 1)
+	{
+		if (kill(pid, SIGUSR1) == -1)
+		{
+			ft_putstr_fd("Fail to send a signal\n", 1);
+			exit(0);
+		}
+	}
+	else
+	{
+		if (kill(pid, SIGUSR2) == -1)
+		{
+			ft_putstr_fd("Fail to send a signal\n", 1);
+			exit(0);
+		}
+	}
+	usleep(100);
+
+}
+
+void	bit_cal(pid_t pid, int num)
+{
+	if (num != 0)
+	{
+		bit_cal(pid, num / 2);
+		send(pid, num % 2);
+	}
+}
+
+void	count_pos(pid_t pid, char c)
+{
+	int	num;
+	int	pos;
+	int i;
+
+	num = (int)c;
+	pos = 1;
+	i = 0;
+	while (num != 1 || num != 0)
+	{
+		num = num / 2;
+		pos++;
+	}
+	while (i < 8 - pos)
+	{
+		send(pid, 0);
+		i++;
+	}
+	num = (int)c;
+	bit_cal(pid, num);
+}
+
+void	send_end(pid_t	pid)
+{
+	int	i;
+	char	c;
+
+	c = '\0';
+	i = 0;
+	while(i++ < 8)
+	{
+		if (c % 2 == 1)
+			send(pid, 1);
+		else
+			send(pid, 0);
+		c >>= 1;
+		usleep(100);
+	}
+}
+
 int main(int argc, char **argv)
 {
+	int i;
+
+	i = 0;
 	if (argc != 3)
 	{
 		ft_putstr_fd("usage : ./client <server pid> <message>\n", 1);
@@ -22,5 +99,11 @@ int main(int argc, char **argv)
 		ft_putstr_fd("Wrong pid range!");
 		return(0);
 	}
+	while (argv[2][i]
+	{
+		count_pos(argv[1], argv[2][i]);
+		i++;
+	}
+	send_end(argv[1]);
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,9 +6,15 @@
 /*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 21:30:53 by hyunjoo           #+#    #+#             */
-/*   Updated: 2022/02/22 21:48:10 by hyunjoo          ###   ########.fr       */
+/*   Updated: 2022/02/23 05:20:16 by hyunjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "server.h"
+
+void	handler()
+{
+}
 
 int main()
 {
@@ -19,7 +25,17 @@ int main()
 	ft_putstr_fd("Server pid : ", 1);
 	ft_putnbr_fd(server_pid, 1);
 	write(1, "\n", 1);
-	act.sa_flags = ;
-	act.sa_mask
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = handler;
+	if (sigaction(SIGUSR1, &act, NULL) != 0)
+	{
+		write(1, "ERROR\n", 6);
+		exit(0);
+	}
+	if (sigaction(SIGUSR2, &act, NULL) != 0)
+	{
+		write(1, "ERROR\n", 6);
+		exit(0);
+	}
 	return (0);
 }
