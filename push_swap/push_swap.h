8f0df4d08b9af5b87b4623683ad71d39abf7b73e
Hyunjoo <jhj2844@naver.com>
2022-04-04 05:49:26 +0900

    push_swap

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 00:36:41 by hyunjoo           #+#    #+#             */
-/*   Updated: 2022/04/04 05:43:57 by hyunjoo          ###   ########.fr       */
+/*   Updated: 2022/04/04 05:49:07 by hyunjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,10 @@ typedef struct  s_stack
   sturck *next;
   int val;
 }
-
-<<<<<<< HEAD
 typedef struct	s_stack
 {
 	struct	*prev;
 	struct	*next;
 	int		val;	
 };
-
-
 #endif
-=======
-#endif
->>>>>>> 764f2317a812ab195c0f01ac835e4a36f7a856d7
