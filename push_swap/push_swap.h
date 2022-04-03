5c81281ceab80169a4ae648fefa38a0464aa4ada
Hyunjoo <jhj2844@naver.com>
2022-04-04 05:03:54 +0900

    1

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyunjoo <hyunjoo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 00:36:41 by hyunjoo           #+#    #+#             */
-/*   Updated: 2022/03/31 00:38:49 by hyunjoo          ###   ########.fr       */
+/*   Updated: 2022/04/02 02:21:02 by hyunjoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
+
+typedef struct	s_stack
+{
+	struct	*prev;
+	struct	*next;
+	int		val;	
+};
+
 
 #endif
