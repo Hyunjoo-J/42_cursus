7f3faa3a57cf95de58b9fb6b3e2b56d2f2ffc48e
J-Hyun-J <jhj2844@naver.com>
2022-04-07 19:46:41 +0900

    push_swap

---
+++ push_swap/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyjeong <hyjeong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 17:59:00 by hyjeong           #+#    #+#             */
+/*   Updated: 2022/04/07 18:18:06 by hyjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# define SUCCESS 1
+# define ERROR 0
+# include <stido.h>
+# include <stdlib.h>
+# include <unstd.h>
+# include <stdbool.h>
+# include "common/common.h"
+# include "sorter/sorter.h"
+#endif
---
+++ push_swap/srcs/sorter/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyjeong <hyjeong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 18:19:50 by hyjeong           #+#    #+#             */
+/*   Updated: 2022/04/07 19:46:21 by hyjeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sorter.h"
+
+void	algorithm(t_mem *mem)
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	t_mem	*mem;
+	int		red_args;
+	if (argc >= 2)
+	{
+		red_args = read_args(argc, argv, mem);
+	}
+}
