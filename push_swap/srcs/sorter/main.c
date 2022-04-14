390258bb38e4028c9aaa9ebe1b3c2e7da8e7474d
J-Hyun-J <jhj2844@naver.com>
2022-04-14 20:45:44 +0900

    push_swap

--- push_swap/srcs/sorter/main.c
+++ push_swap/srcs/sorter/main.c
@@ -6,7 +6,7 @@
 /*   By: hyjeong <hyjeong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 18:19:50 by hyjeong           #+#    #+#             */
-/*   Updated: 2022/04/14 19:57:03 by hyjeong          ###   ########.fr       */
+/*   Updated: 2022/04/14 20:45:32 by hyjeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 
 void	algorithm(t_mem *mem)
 {
+	t_stack		*cpy;
+	t_params	params;
 
+	params = init_params(mem);
+	cpy = cpy_stack(mem->a, mem, &params);
+	bubble_sort_it(cpy);
+	set_target_index_cpy(cpy);
+	set_target_index(cpy);
+	free_stack(cpy);
+	set_keep(mem);
+	push_keep_false_b(mem);
+	while (!is_empty_stack(mem->b))
+	{
+		params.elem = mem->b->first;
+		evaluate_actions(mem, &params);
+		sort(mem, &params);
+	}
+	if (!(is_on_top(mem->a, smallest(mem->a))))
+		bring_to_top(mem->a, smallest(mem->a), mem);
+	if (is_empty_stack(mem->a))
+		error(mem, 1, NULL, NULL);
+	free_all(mem, NULL, NULL);
 }
 
 int	main(int argc, char **argv)
